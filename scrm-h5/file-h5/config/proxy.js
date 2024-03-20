8d2ce2549004a2e9587bbfd263905c62594f8f87
lyonru29 <835318656@qq.com>
2024-03-20 20:03:43 +0800

    remove file-h5

--- scrm-h5/file-h5/config/proxy.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '/api': {
-    target: 'http://127.0.0.1:9900',
-    pathRewrite: { '^': '' },
-  },
-};
--- scrm-h5/file-h5/src/data.js
+++
@@ -1,9 +0,0 @@
-import { TEST_DOMAIN } from "./constants";
-export default {
-  isMock: window.isMock,
-  userInfo: {},
-  baseHost: window.isMock
-    ? TEST_DOMAIN
-    : window.location.origin,
-  fileInfo: {},
-};
